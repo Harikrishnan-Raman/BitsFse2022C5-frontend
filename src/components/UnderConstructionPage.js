8c374847e1256a2f55a3598df898578b0c7e5682
Harikrishnan Raman <58678058+Harikrishnan-Raman@users.noreply.github.com>
2022-07-01 22:14:54 +0530

    Update UnderConstructionPage.js

--- src/components/UnderConstructionPage.js
+++ src/components/UnderConstructionPage.js
@@ -3,7 +3,7 @@ export class UnderConstructionPage extends Component{
     render(){
         return(
         <div className="under-construction-page-container">
-            <div className="under-construction">The Page is Under Construction</div>
+            <div className="under-construction">The Page is Under Construction, Thanks !</div>
         </div>
         )
     }
